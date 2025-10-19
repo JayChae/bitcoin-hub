00b9b61f00b22facc6a80612292a90e10fde5d7c
MR.J <cowhdals1111@gmail.com>
2025-10-19 23:36:44 +0900

    update coming-soon component

--- components/coming-soon.tsx
+++ components/coming-soon.tsx
@@ -4,10 +4,18 @@ export default async function ComingSoon() {
   const t = await getTranslations("comingSoon");
 
   return (
-    <div className="flex min-h-[50vh] flex-col items-center justify-center space-y-4 p-4 text-center">
-      <h1 className="text-4xl font-bold tracking-tight">{t("title")}</h1>
-      <div className="h-1 w-20 rounded-full bg-gray-200" />
-      <p className="text-lg text-gray-600">🚧 {t("description")} 🚧</p>
-    </div>
+    <section className="flex items-center px-4 pt-24 sm:block sm:h-fit lg:px-8">
+      <div className="mx-auto max-w-4xl text-center">
+        <h1 className="from-primary to-secondary mb-6 bg-gradient-to-r bg-clip-text text-3xl font-bold text-transparent sm:text-4xl md:text-5xl">
+          {t("title")}
+        </h1>
+        <p className="text-muted-foreground mx-auto mb-8 max-w-2xl text-lg sm:text-xl">
+          🚧 {t("subtitle")} 🚧
+        </p>
+        <p className="text-muted-foreground mx-auto mb-8 max-w-2xl text-xs sm:text-sm">
+          {t("description")}
+        </p>
+      </div>
+    </section>
   );
 }
