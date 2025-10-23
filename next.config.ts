384e4f97911d3d02e4c2683d4d1b727d3e181aad
MR.J <cowhdals1111@gmail.com>
2025-10-23 15:26:08 +0900

    Enable SVG support in Next.js configuration by setting dangerouslyAllowSVG to true for improved image handling.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   images: {
+    dangerouslyAllowSVG: true,
     remotePatterns: [
       {
         protocol: "https",
