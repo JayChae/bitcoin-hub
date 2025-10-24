b4cb3654b58d5d5957fbd69033e7178dea2439d4
MR.J <cowhdals1111@gmail.com>
2025-10-24 09:13:51 +0900

    add translation

--- app/[locale]/development/bitcoin/_resources/software-wallets.ts
+++ app/[locale]/development/bitcoin/_resources/software-wallets.ts
@@ -6,133 +6,119 @@ export const softwareWalletsEn: BitcoinDevResources["software-wallets"] = [
     description:
       "The original Bitcoin wallet, also serving as a full node, that provides high levels of security, privacy, and stability.",
     url: "https://bitcoincore.org",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/13464320?s=200&v=4",
   },
   {
     name: "Blockstream Green",
     description:
       "Mobile and desktop wallet featuring multi-signature security, hardware wallet support, and integration with the Liquid Network.",
     url: "https://blockstream.com/green",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/7424983?s=200&v=4",
   },
   {
     name: "Blue Wallet",
     description:
       "Mobile-focused Bitcoin and Lightning wallet designed for ease of use, featuring a user-friendly interface and multiple storage options.",
     url: "https://bluewallet.io",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/43582003?s=200&v=4",
   },
   {
     name: "Electrum",
     description:
       "Lightweight desktop Bitcoin wallet that offers speed and low resource usage, while also providing advanced features like cold storage and multi-signature support.",
     url: "https://electrum.org",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/2084673?s=200&v=4",
   },
   {
     name: "Nunchuk",
     description:
       "Mobile multisig Bitcoin wallet offering state-of-the-art security features, ease of use, and a variety of collaborative custody options.",
     url: "https://nunchuk.io/",
-    logo: "",
+    logo: "https://pbs.twimg.com/profile_images/1584226683133255680/Dt-HDueM_400x400.jpg",
   },
   {
     name: "Sparrow Wallet",
     description:
       "Bitcoin wallet that focuses on transparency and usability, featuring advanced features like coin selection and multisig setups.",
     url: "https://www.sparrowwallet.com",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/64004273?s=200&v=4",
   },
   {
     name: "Fully Noded",
     description:
       "Mobile wallet designed for advanced users, allowing full control over a Bitcoin Core node via its intuitive interface.",
     url: "https://github.com/Fonta1n3/FullyNoded",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/30832395?v=4",
   },
   {
     name: "Wasabi Wallet",
     description:
       "Privacy-focused Bitcoin wallet that integrates CoinJoin technology for enhanced anonymity.",
     url: "https://wasabiwallet.io/",
-    logo: "",
-  },
-  {
-    name: "Samourai Wallet",
-    description:
-      "Android wallet focused on privacy and security, featuring built-in VPN and Tor support, as well as advanced transaction options.",
-    url: "https://samouraiwallet.com",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/170895068?s=200&v=4",
   },
   {
     name: "Wallet of Satoshi",
     description:
       "User-friendly and accessible Lightning Network wallet that allows for instant, fee-minimal Bitcoin transactions, aiming to provide a simple interface for entering into the world of Bitcoin and Lightning payments.",
     url: "https://www.walletofsatoshi.com",
-    logo: "",
+    logo: "https://pbs.twimg.com/profile_images/1711296158021431296/bs1nK5N4_400x400.png",
   },
   {
     name: "Muun Wallet",
     description:
       "Mobile Bitcoin and Lightning wallet aiming for simplicity and ease of use, featuring seamless Lightning Network integration for fast and low-fee transactions.",
     url: "https://muun.com/",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/10998166?s=200&v=4",
   },
   {
     name: "BitKit",
     description:
       "Comprehensive Bitcoin toolkit that goes beyond being just a wallet offering features that allow users to have control over their money, profiles, contacts, and web accounts.",
     url: "https://bitkit.to",
-    logo: "",
+    logo: "https://pbs.twimg.com/profile_images/1804056794781642752/nZ78il7V_400x400.png",
   },
   {
     name: "Aqua",
     description:
       "Non-custodial wallet that lets you save, spend, and buy Bitcoin, Liquid assets, and USDt offering low fees, high privacy, and access to a range of services like loans, debit card, and interest-bearing products.",
     url: "https://aquawallet.io/",
-    logo: "",
+    logo: "https://pbs.twimg.com/profile_images/1857669644678737928/1pBkBJjp_400x400.png",
   },
   {
     name: "My Celium",
     description:
       "Mobile Bitcoin wallet that offers advanced privacy and security features, as well as hardware wallet integration and backup options.",
     url: "https://wallet.mycelium.com",
-    logo: "",
+    logo: "https://wallet.mycelium.com/images/Untitled-3.svg",
   },
   {
     name: "Coin.in wallet",
     description:
       "Free, open-source Bitcoin wallet and toolkit designed for learning, featuring offline address creation, multisignature support, and privacy-enhanced transactions with TOR support.",
     url: "https://coinb.in",
-    logo: "",
+    logo: "https://coinb.in/images/coinbin.png",
   },
   {
     name: "Padawan Wallet",
     description:
       "Free, open-source Bitcoin wallet opearting on Bitcoin's testnet, allowing users to experiment and learn about Bitcoin in a risk-free environment.",
     url: "https://padawanwallet.com",
-    logo: "",
+    logo: "https://pbs.twimg.com/profile_images/1618269331934126080/ClkhdJnI_400x400.jpg",
   },
   {
     name: "BancoLibre",
     description:
       "Free, open-source self-custodial bitcoin web wallet that prioritizes your security while ensuring a seamless and user-friendly experience.",
     url: "https://bancolibre.com/docs#what-is-bancolibre",
-    logo: "",
-  },
-  {
-    name: "BlindBitD",
-    description:
-      "Free, open-source Bip352 silent payment wallet which runs as daemon written in GO.",
-    url: "https://bancolibre.com/docs#what-is-bancolibre",
-    logo: "",
+    logo: "https://placehold.co/80x80?text=BancoLibre",
   },
   {
     name: "Bitcoin Safe",
     description:
       "Free, open-source multi-language Bitcoin savings wallet written in python that offers secure single-signature and multi-signature setups, featuring modern visualizations, hardware wallet compatibility, and synchronization capabilities.",
     url: "https://bitcoin-safe.org",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/116060138?v=4",
   },
 ];
 
@@ -142,132 +128,118 @@ export const softwareWalletsKo: BitcoinDevResources["software-wallets"] = [
     description:
       "비트코인의 원본 지갑으로, 전체 노드로서도 작동하며 높은 수준의 보안, 프라이버시, 안정성을 제공합니다.",
     url: "https://bitcoincore.org",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/13464320?s=200&v=4",
   },
   {
     name: "Blockstream Green",
     description:
       "다중 서명 보안, 하드웨어 지갑 지원, 그리고 리퀴드 네트워크와의 통합을 특징으로 하는 모바일 및 데스크톱 지갑입니다.",
     url: "https://blockstream.com/green",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/7424983?s=200&v=4",
   },
   {
     name: "Blue Wallet",
     description:
       "사용 편의성을 위해 설계된 모바일 중심의 비트코인 및 라이트닝 지갑으로, 사용자 친화적인 인터페이스와 다양한 저장 옵션을 제공합니다.",
     url: "https://bluewallet.io",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/43582003?s=200&v=4",
   },
   {
     name: "Electrum",
     description:
       "빠른 속도와 낮은 리소스 사용량을 제공하는 경량 데스크톱 비트코인 지갑으로, 콜드 스토리지 및 다중 서명 지원과 같은 고급 기능도 제공합니다.",
     url: "https://electrum.org",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/2084673?s=200&v=4",
   },
   {
     name: "Nunchuk",
     description:
       "최신 보안 기능, 사용 편의성, 그리고 다양한 협력적 보관 옵션을 제공하는 모바일 멀티시그 비트코인 지갑입니다.",
     url: "https://nunchuk.io/",
-    logo: "",
+    logo: "https://pbs.twimg.com/profile_images/1584226683133255680/Dt-HDueM_400x400.jpg",
   },
   {
     name: "Sparrow Wallet",
     description:
       "투명성과 사용성을 중점으로 하는 비트코인 지갑으로, 코인 선택 및 멀티시그 설정과 같은 고급 기능을 제공합니다.",
     url: "https://www.sparrowwallet.com",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/64004273?s=200&v=4",
   },
   {
     name: "Fully Noded",
     description:
       "고급 사용자를 위해 설계된 모바일 지갑으로, 직관적인 인터페이스를 통해 비트코인 코어 노드를 완전히 제어할 수 있도록 합니다.",
     url: "https://github.com/Fonta1n3/FullyNoded",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/30832395?v=4",
   },
   {
     name: "Wasabi Wallet",
     description:
       "개인정보 보호에 중점을 둔 비트코인 지갑으로, 향상된 익명성을 위한 CoinJoin 기술을 통합합니다.",
     url: "https://wasabiwallet.io/",
-    logo: "",
-  },
-  {
-    name: "Samourai Wallet",
-    description:
-      "개인정보 보호와 보안에 중점을 둔 안드로이드 지갑으로, 내장 VPN과 Tor 지원, 그리고 고급 트랜잭션 옵션을 제공합니다.",
-    url: "https://samouraiwallet.com",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/170895068?s=200&v=4",
   },
   {
     name: "Wallet of Satoshi",
     description:
       "사용자 친화적이고 접근하기 쉬운 라이트닝 네트워크 지갑으로, 즉시적이고 수수료가 최소화된 비트코인 트랜잭션을 가능하게 하며, 비트코인과 라이트닝 결제의 세계에 진입할 수 있도록 단순한 인터페이스를 제공하는 것을 목표로 합니다.",
     url: "https://www.walletofsatoshi.com",
-    logo: "",
+    logo: "https://pbs.twimg.com/profile_images/1711296158021431296/bs1nK5N4_400x400.png",
   },
   {
     name: "Muun Wallet",
     description:
       "단순성과 사용 편의성을 목표로 하는 모바일 비트코인 및 라이트닝 지갑으로, 빠르고 낮은 수수료의 트랜잭션을 위한 원활한 라이트닝 네트워크 통합을 특징으로 합니다.",
     url: "https://muun.com/",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/10998166?s=200&v=4",
   },
   {
     name: "BitKit",
     description:
       "단순한 지갑을 넘어서는 종합 비트코인 툴킷으로, 사용자가 자신의 돈, 프로필, 연락처, 웹 계정에 대한 통제력을 가질 수 있도록 하는 기능을 제공합니다.",
     url: "https://bitkit.to",
-    logo: "",
+    logo: "https://pbs.twimg.com/profile_images/1804056794781642752/nZ78il7V_400x400.png",
   },
   {
     name: "Aqua",
     description:
       "저수수료, 높은 프라이버시, 그리고 대출, 직불카드, 이자 지급 상품과 같은 다양한 서비스에 대한 접근을 제공하는 비트코인, 리퀴드 자산, USDt를 저장, 지출, 구매할 수 있도록 하는 비보관 지갑입니다.",
     url: "https://aquawallet.io/",
-    logo: "",
+    logo: "https://pbs.twimg.com/profile_images/1857669644678737928/1pBkBJjp_400x400.png",
   },
   {
     name: "My Celium",
     description:
       "고급 프라이버시 및 보안 기능, 하드웨어 지갑 통합, 백업 옵션을 제공하는 모바일 비트코인 지갑입니다.",
     url: "https://wallet.mycelium.com",
-    logo: "",
+    logo: "https://wallet.mycelium.com/images/Untitled-3.svg",
   },
   {
     name: "Coin.in wallet",
     description:
       "학습을 위해 설계된 무료 오픈소스 비트코인 지갑 및 툴킷으로, 오프라인 주소 생성, 다중 서명 지원, TOR 지원을 통한 개인정보 보호 강화 트랜잭션 기능을 제공합니다.",
     url: "https://coinb.in",
-    logo: "",
+    logo: "https://coinb.in/images/coinbin.png",
   },
   {
     name: "Padawan Wallet",
     description:
       "비트코인의 테스트넷에서 작동하는 무료 오픈소스 비트코인 지갑으로, 사용자가 위험 없는 환경에서 비트코인을 실험하고 배울 수 있도록 합니다.",
     url: "https://padawanwallet.com",
-    logo: "",
+    logo: "https://pbs.twimg.com/profile_images/1618269331934126080/ClkhdJnI_400x400.jpg",
   },
   {
     name: "BancoLibre",
     description:
       "사용자의 보안을 최우선으로 하면서도 원활하고 사용자 친화적인 경험을 보장하는 무료 오픈소스 자체 보관 비트코인 웹 지갑입니다.",
     url: "https://bancolibre.com/docs#what-is-bancolibre",
-    logo: "",
-  },
-  {
-    name: "BlindBitD",
-    description:
-      "GO로 작성된 데몬으로 실행되는 무료 오픈소스 Bip352 무소음 결제 지갑입니다.",
-    url: "https://bancolibre.com/docs#what-is-bancolibre",
-    logo: "",
+    logo: "https://placehold.co/80x80?text=BancoLibre",
   },
   {
     name: "Bitcoin Safe",
     description:
       "파이썬으로 작성된 무료 오픈소스 다국어 비트코인 저축 지갑으로, 안전한 단일 서명 및 다중 서명 설정을 제공하며, 현대적인 시각화, 하드웨어 지갑 호환성, 동기화 기능을 특징으로 합니다.",
     url: "https://bitcoin-safe.org",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/116060138?v=4",
   },
 ];
