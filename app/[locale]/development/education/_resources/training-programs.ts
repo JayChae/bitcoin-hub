a4adbbdc9698dc96131e6a882794fd9261af43e7
MR.J <cowhdals1111@gmail.com>
2025-10-26 14:21:59 +0900

    add programs logo

--- app/[locale]/development/education/_resources/training-programs.ts
+++ app/[locale]/development/education/_resources/training-programs.ts
@@ -6,42 +6,42 @@ export const trainingProgramsEn: DevResource[] = [
     description:
       "Intensive program aimed at onboarding developers into the Bitcoin ecosystem, occasionally providing stipends.",
     url: "https://residency.chaincode.com",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/8126973?s=200&v=4",
   },
   {
     name: "Summer Of Bitcoin",
     description:
       "Global, online summer internship program focused on introducing university students to bitcoin open-source development and design.",
     url: "https://www.summerofbitcoin.org",
-    logo: "",
+    logo: "https://pbs.twimg.com/profile_images/1450717705632301057/BWfeOPU__400x400.jpg",
   },
   {
     name: "Sovereign Engineering",
     description:
       "Innovative program offering a two-month immersive experience for developers in the Bitcoin and Nostr ecosystems, focusing on building applications, mentorship, and fostering innovation for a self-sovereign future.",
     url: "https://sovereignengineering.io",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/141315839?s=200&v=4",
   },
   {
     name: "Bitcoin Scholarship",
     description:
       "100% of college tuition covered to encourage the brightest minds to contribute to bitcoin open source development.",
     url: "https://bitcoinscholarship.xyz",
-    logo: "",
+    logo: "https://bitcoinscholarship.xyz/public/mortarboard_192x192.png",
   },
   {
     name: "Bitcoin Students Network",
     description:
       "One-Stop-Shop for students interested in learning about Bitcoin and contributing to the space where students can get connected to resources (logistical, educational & financial), opportunities (educational, job & skills-based), and importantly, to each other.",
     url: "https://www.bitcoinstudentsnetwork.org/",
-    logo: "",
+    logo: "https://pbs.twimg.com/profile_images/1763505393697718272/IxY2nW9R_400x400.jpg",
   },
   {
     name: "Vinteum",
     description:
       "Non-profit Bitcoin research and development center dedicated to supporting Bitcoin developers in Brazil and the wider Latin America region.",
     url: "https://vinteum.org",
-    logo: "",
+    logo: "https://vinteum.org/wp-content/uploads/2022/06/logo_vinteum-e1654186269199-1024x171.png",
   },
 ];
 
@@ -50,41 +50,41 @@ export const trainingProgramsKo: DevResource[] = [
     name: "Chaincode Labs Residency",
     description: "비트코인 생태계 개발자 온보딩 집중 프로그램. 보조금 지급.",
     url: "https://residency.chaincode.com",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/8126973?s=200&v=4",
   },
   {
     name: "Summer Of Bitcoin",
     description:
       "대학생 비트코인 오픈소스 개발 및 디자인 소개. 글로벌 온라인 여름 인턴십 프로그램.",
     url: "https://www.summerofbitcoin.org",
-    logo: "",
+    logo: "https://pbs.twimg.com/profile_images/1450717705632301057/BWfeOPU__400x400.jpg",
   },
   {
     name: "Sovereign Engineering",
     description:
       "비트코인 및 Nostr 생태계 개발자 대상 2개월 몰입형 경험. 애플리케이션 구축, 멘토십, 자주권 미래 혁신.",
     url: "https://sovereignengineering.io",
-    logo: "",
+    logo: "https://avatars.githubusercontent.com/u/141315839?s=200&v=4",
   },
   {
     name: "Bitcoin Scholarship",
     description:
       "비트코인 오픈소스 개발 기여 장려. 우수 인재 대학 등록금 100% 지원.",
     url: "https://bitcoinscholarship.xyz",
-    logo: "",
+    logo: "https://bitcoinscholarship.xyz/public/mortarboard_192x192.png",
   },
   {
     name: "Bitcoin Students Network",
     description:
       "비트코인 학습 및 기여 관심 학생 원스톱 서비스. 학생 연결 자원 (물류, 교육, 재정), 기회 (교육, 취업, 기술), 상호 연결.",
     url: "https://www.bitcoinstudentsnetwork.org/",
-    logo: "",
+    logo: "https://pbs.twimg.com/profile_images/1763505393697718272/IxY2nW9R_400x400.jpg",
   },
   {
     name: "Vinteum",
     description:
       "브라질 및 광범위 라틴아메리카 지역 비트코인 개발자 지원 비영리 연구개발 센터.",
     url: "https://vinteum.org",
-    logo: "",
+    logo: "https://vinteum.org/wp-content/uploads/2022/06/logo_vinteum-e1654186269199-1024x171.png",
   },
 ];
